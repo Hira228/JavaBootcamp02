73763a1879e80c5f70f51a68b9f48b5138330405
Grady Zankale <gradyzan@at-g1.msk.21-school.ru>
2024-01-19 19:39:59 +0300

    asdfa

---
+++ src/ex02.java
@@ -0,0 +1,55 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.*;
+import java.io.File;
+
+public class ex02 {
+
+    public static void main(String[] args) {
+        File currentDir = new File(args[0]);
+        Scanner scanner = new Scanner(System.in);
+        ChoiceCommands(scanner, currentDir);
+
+
+    }
+
+    public static void ChoiceCommands(Scanner scanner, File currentDir) {
+        while (scanner.hasNextLine()) {
+            String str = scanner.nextLine();
+            switch (str.split(" ")[0]) {
+                case ("ls"):
+                    for(String file : Objects.requireNonNull(currentDir.list())) {
+                        File curFile = new File(currentDir + "/" + file);
+                        if (curFile.isDirectory()) {
+                            long sum = 0;
+                            File[] curFiles = curFile.listFiles();
+                            for (File f : Objects.requireNonNull(curFiles)) {
+                                sum += f.length() / 1024;
+                            }
+                            System.out.println(file + " " + sum + " KB");
+                        } else System.out.println(file + " " + (new File(currentDir + "/" + file).length()) / 1024 + " KB");
+                    }
+                    break;
+
+                case ("cd"):
+                    String[] path = str.split(" ")[1].split("/");
+                    for(String part : path) {
+
+                    }
+                    if ()
+
+                case ("mv"):
+                    System.out.println("это mv");
+                    break;
+
+                case ("exit"):
+                    System.exit(0);
+
+                default:
+                    System.out.println("Давай заново");
+                    ChoiceCommands(scanner, currentDir);
+                    break;
+            }
+        }
+    }
+}
